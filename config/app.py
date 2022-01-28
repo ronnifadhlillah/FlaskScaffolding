091f1716b5769994246f6fc551835d5087a8111d
ronnifadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2022-01-28 09:55:40 +0700

    Change concept of authentication
    Change authenticationn concept and make authentication model.

---
+++ config/app.py
@@ -0,0 +1,22 @@
+[Application]
+  AppName=Scaffolding
+  Debug=True
+  Environment=development
+  Timezone=UTC
+  Locale=id
+  FallbackLocale=en
+  SecretKey=b'\xf3\xacD\x07I\x07d\x05}{\xf5\xa0M6.\x1c\xff?.\x8d\xa2\xbe\xaeW'
+
+[URI]
+# For development in different port or url
+  Set=False
+  Url=127.0.0.1
+  Port=3000
+
+[BootStrap]
+  Template=views
+  Static=Public
+  Router=Route
+
+[Auth]
+  ExpireIn=60
